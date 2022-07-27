07f32d1a5fd8cd9794e11ad3dced1698020eaaff
Paul McCarty <paul@x-plane.com>
2022-07-27 17:06:37 -0500

    Corrects .xpl file name in archive.sh

--- jenkins/archive.sh
+++ jenkins/archive.sh
@@ -2,13 +2,13 @@
 
 case "$PLATFORM" in
 "IBM")
-	echo jenkins/build_products/lua_win.xpl
+	echo jenkins/build_products/xlua_win.xpl
 	;;
 "APL")
-	echo jenkins/build_products/lua_mac.xpl
+	echo jenkins/build_products/xlua_mac.xpl
 	;;
 "LIN")
-	echo jenkins/build_products/lua_lin.xpl
+	echo jenkins/build_products/xlua_lin.xpl
 	;;
 esac
 
