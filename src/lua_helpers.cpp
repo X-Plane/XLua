5ea20022b297af203ff4aba641b94574b0226aa3
Jim Keir <jimkeir@users.noreply.github.com>
2026-03-25 18:10:53 +0000

    Revert "Remove SIM_PERIOD and IN_REPLAY lua variables mirroring existing datarefs"
    This reverts commit e42e23dbe7a96ee78d0628b955d3e3dfea428e30.

--- src/lua_helpers.cpp
+++ src/lua_helpers.cpp
@@ -18,6 +18,7 @@
 
 #include "log.h"
 #include "xpfuncs.h"
+#include "module.h"
 
 extern XPLMDataRef				g_replay_active;
 extern XPLMDataRef				g_sim_period;
@@ -102,6 +103,19 @@ int lua_pushtraceback(lua_State * L)
 {
 	lua_pushcfunction(L, traceback);
 	return lua_gettop(L);
+}
+
+void setup_std_vars(lua_State * L, int dbg)
+{
+	::module const* mod = module::module_from_interp(L);
+	if (mod != nullptr && mod->get_required_version()[0] == 1)
+	{
+		lua_pushnumber(L, XPLMGetDataf(g_sim_period));
+		lua_setglobal(L, "SIM_PERIOD");
+
+		lua_pushnumber(L, XPLMGetDatai(g_replay_active));
+		lua_setglobal(L, "IN_REPLAY");
+	}
 }
 
 template<>
